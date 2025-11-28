0d29b65eb8f5b3b9b9c3a6adfb6568eb8c9b8a65
hellybates <119062412+hellybates@users.noreply.github.com>
2025-11-28 15:42:47 +0000

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -447,7 +447,7 @@ class MarkdownLoader {
 
         const bee = document.createElement('div');
         bee.className = 'flying-bee';
-        bee.textContent = '🐝';
+        bee.textContent = '☄️';
         bee.style.left = startX + 'px';
         bee.style.top = startY + 'px';
 
